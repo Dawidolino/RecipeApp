2fbcbd1cedf9c9457410f1302902b4c59b8be950
Dawid Ruth <dawidolinp1@gmail.com>
2024-11-17 17:04:49 +0100

    add models

--- RecipeApp/Models/Ingredient.cs
+++ RecipeApp/Models/Ingredient.cs
@@ -1,7 +1,19 @@
-﻿namespace RecipeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeApp.Models
 {
     public class Ingredient
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        public string IngredientName { get; set; }
+        [StringLength(500,ErrorMessage ="Opis powinien zawierać maksymalnie 500 znaków")]
+        public string IngredientDescription { get; set; }
+
+        //many to many
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+
     }
 }
---
+++ RecipeApp/Models/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApp.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Podaj nazwę przepisu")]
+        public string RecipeName { get; set; }
+
+        [Required]
+        [StringLength(1500, ErrorMessage ="Opis nie może przekraczać 1500 znaków")]
+        public string RecipeDescription { get; set; }
+
+        [Required]
+        public string RecipeCategory { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        [Range(1, 1000, ErrorMessage ="Czas przygotowania musi być w przedziale 1-1000 minut")]
+        public int? PrepTime { get; set; }
+      
+        //many to many
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+
+    }
+}
