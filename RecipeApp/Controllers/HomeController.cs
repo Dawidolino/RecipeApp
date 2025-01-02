f61fc2f56bd135e814ee62731e73125399fd712c
Dawid Ruth <dawidolinp1@gmail.com>
2025-01-02 03:08:56 +0100

    change home page to recipe list

--- RecipeApp/Controllers/HomeController.cs
+++ RecipeApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeApp.Data;
 using RecipeApp.Models;
 using System.Diagnostics;
 
@@ -6,27 +8,17 @@ namespace RecipeApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var recipes = await _context.Recipes.Include(r => r.RecipeIngredients).ToListAsync();
+            return View(recipes);
         }
     }
 }
